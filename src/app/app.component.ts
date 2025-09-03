0ab47e0873e3d2d6b7a7dfa61ba450794f03262b
matt higg <mhh129@gmail.com>
2025-09-03 17:35:05 -0400

    Update app component to store log data in a signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BattleService } from './services/battle.service';
 import { CommonModule } from '@angular/common';
@@ -6,12 +6,21 @@ import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  // imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  log: any;
+  mockData = {
+    epoch: 'start epoch',
+    winner: 'start winner',
+    reward_A: 'start reward a',
+    reward_B: 'start reward b',
+    wins: 'start wins',
+  }
+
+  log = signal(this.mockData)
 
   constructor(private battleService: BattleService) {}
 
@@ -23,9 +32,16 @@ export class AppComponent {
     const eventSource = this.battleService.getTrainingStream();
 
     eventSource.onmessage = (event) => {
-      const log = JSON.parse(event.data);
-      console.log("Training update:", log);
-      this.log = log;
+      const data = JSON.parse(event.data);
+
+      if (data.status === "completed") {
+        console.log("Training finished");
+        eventSource.close();
+        return;
+      }
+
+      this.log.set({ ...data }); 
+      console.log("UI rendering log:", this.log());
     };
 
     eventSource.onerror = (error) => {
