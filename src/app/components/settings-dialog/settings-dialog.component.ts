80429828ad93f0f8ec3263a7fe3811cb6b75865b
matt higg <mhh129@gmail.com>
2025-10-11 20:27:37 -0400

    Update settings-dialog & dashboard components to move color themes to settings-dialog component

--- src/app/components/settings-dialog/settings-dialog.component.ts
+++ src/app/components/settings-dialog/settings-dialog.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ColorThemeService } from '../../services/color-theme/color-theme.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-settings-dialog',
-  imports: [],
+  imports: [
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './settings-dialog.component.html',
   styleUrl: './settings-dialog.component.scss'
 })
 export class SettingsDialogComponent {
+  public colorThemeService: ColorThemeService = inject(ColorThemeService);
 
+  get colorThemeFC(): FormControl {
+    return this.colorThemeService.colorThemeFormGroup.get('colorThemeFC') as FormControl;
+  }
+
+  get lightOrDarkFC(): FormControl {
+    return this.colorThemeService.colorThemeFormGroup.get('lightOrDarkFC') as FormControl;
+  }
 }
